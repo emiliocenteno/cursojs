0c6e0092d5ccba2e80f906b2f86158316fdd6bd3
rcenteno <rc@centeno.pe>
2021-02-16 11:56:49 -0500

    se termino la 3ra funcion que devuelve un array

--- funciones.js
+++ funciones.js
@@ -4,17 +4,15 @@
 function cuentaCaracteres(cadena) {
 
     let cuenta;
-    let length =0 ;
+    let length = 0;
     cuenta = cadena.length
 
-
-   /// length = 0;
-    while (cadena[length] !== undefined) {
+   while (cadena[length] !== undefined) {
         length++
     }
     console.log(`==================================EJERCICIO 1==================================`);
     console.log(`cuentaCarcadena (${cadena}) Resultado: ${length}`);
- 
+
 }
 
 cuentaCaracteres("Hola Mundo");
@@ -23,23 +21,55 @@ cuentaCaracteres("Hola Mundo");
 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 */
 
-function textoRecortado(cadena, corte ){
+function textoRecortado(cadena, corte) {
 
-let length =0;
-  /// length = 0;
-  let subcadena;
-  while (cadena[length] !== undefined) {
-  
-    if (length == corte) { 
-        console.log(cadena[length] )
-        break; 
+    let length = 0;
+    let subcadena;
+    while (cadena[length] !== undefined) {
+
+        if (length == corte) {
+            console.log(cadena[length])
+            break;
+        }
+        length++
+        subcadena = cadena.substring(0, length);
     }
-    length++
-    subcadena= cadena.substring(0,length);
-}
-console.log(`==================================EJERCICIO 2==================================`)
-//console.log(subcadena)
-console.log(`textoRecortado(${cadena},${corte}) Resultado: ${length}`);
+    console.log(`==================================EJERCICIO 2==================================`)
+    console.log(`textoRecortado(${cadena},${corte}) Resultado: ${subcadena}`);
 }
 
-textoRecortado("hola a todos",4)
+textoRecortado("hola a todos", 4)
+
+
+/*
+3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].*/
+function devuelveArray(cadena, ciertoCaracter) {
+    let length = 0;
+    let s = new Array();
+    let c
+    let ind = 0;
+    let x = "";
+
+    while (cadena[length] !== undefined) {
+    
+        if (cadena[length] !== ciertoCaracter) {
+            x = x + cadena[length];
+        }
+        else {
+            ind++
+            x = '';
+        }
+        ///var score = pattern.Gotoccurance.score !=null ? pattern.Gotoccurance.score : '0';
+    
+        s[ind] = x;
+      
+        length++
+    }
+    console.log(`==================================EJERCICIO 3==================================`)
+    console.log(`devuelveArray(${cadena},${ciertoCaracter}) Resultado: ${s}`);
+   // console.log(s);
+}
+devuelveArray('hola que tal', ' ');
+
+/*
+4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.*/ 
